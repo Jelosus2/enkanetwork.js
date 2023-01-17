62f60921e629e3be26f401369803ba16d36fa359
Jelosus2 <navarrorodriguezjesus6@gmail.com>
2023-01-17 14:36:42 +0100

    Delete src/structs/AssetNames directory

--- src/structs/AssetNames/NamecardHash.ts
+++
@@ -1,9 +0,0 @@
-import { namecards } from "../../utils/namecards"
-
-export class NamecardHash {
-  value: string | string
-  
-  constructor(namecardId: string | number) {
-    this.value = namecards[namecardId] ? namecards[namecardId].nameTextMapHash : 'not found'
-  }
-}
--- src/structs/AssetNames/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './AssetName'
-export * from './NamecardHash'
-export * from './SkillHash'
-export * from './TalentHash'
